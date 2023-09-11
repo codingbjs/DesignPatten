ad9095ff450b03751ad0a9e05756968675ad1b3e
codingbjs <codingbjs@gmail.com>
2023-09-11 10:06:26 +0900

    template Patten

--- template/Dao.py
+++ template/Dao.py
@@ -13,6 +13,17 @@ class Dao:
         finally:
             self.close(cursor, conn)
 
+    def insert(self, sql, data):
+        try:
+            conn = self.get_connection()
+            cursor = conn.cursor()
+            cursor.executemany(sql, data)
+            conn.commit()
+        except:
+            conn.rollback()
+        finally:
+            self.close(cursor, conn)
+
     def close(self, cursor, conn):
         cursor.close()
         conn.close()
--- template/main.py
+++ template/main.py
@@ -2,4 +2,12 @@ from Dao import *
 
 if __name__ == '__main__':
     dao = Dao()
-    dao.create('CREATE TABLE users (id integer primary key, name text, age integer)')
+    # dao.create('CREATE TABLE users (id integer primary key, name text, age integer)')
+    users = [
+        ("hmd", 73),
+        ("cyy", 12),
+        ("hol", 48),
+        ("htg", 87)
+    ]
+
+    # dao.insert('INSERT INTO users (name, age) VALUES (?, ?)', users)
